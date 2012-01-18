173cac9c506488dd4d049c84964631c778efb19e
Nic Pillinger <nic@agilex.co.uk>
2012-01-18 11:33:09 +0000

    add openrasta config

---
+++ src/agilex.logging.openrasta/AgilexOpenRasta.cs
@@ -0,0 +1,34 @@
+﻿using OpenRasta.Configuration;
+using OpenRasta.DI;
+using OpenRasta.Diagnostics;
+using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Layout;
+
+namespace agilex.logging.openrasta
+{
+    public static class AgilexOpenRasta
+    {
+        public static void ConfigureLogging(string logfilename, string logname)
+        {
+            BasicConfigurator.Configure(
+                new RollingFileAppender
+                    {
+                        Layout = new PatternLayout("%date [%thread] %-5level %c - %message%newline"),
+                        File = logfilename,
+                        AppendToFile = true,
+                        Name = logname,
+                        RollingStyle = RollingFileAppender.RollingMode.Size,
+                        MaximumFileSize = "10MB",
+                        MaxSizeRollBackups = 1,
+                        StaticLogFileName = true
+                    }
+                );
+            ResourceSpace.Uses.Resolver.AddDependencyInstance<ILog>(
+                LogManager.GetLogger(logname), DependencyLifetime.Singleton);
+            ResourceSpace.Uses.CustomDependency
+                <ILogger, Log4NetLogger>(DependencyLifetime.Singleton);
+        }
+    }
+}
